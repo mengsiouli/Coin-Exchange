14380cf0b3513bf8714b5177827e438b787f2ead
msli <msli.leo.work@gmail.com>
2026-02-12 16:08:28 +0800

    修正 註解

--- CoinExchange/Program.cs
+++ CoinExchange/Program.cs
@@ -7,21 +7,18 @@ namespace CoinExchange
 	internal static class Program
 	{
 		/// <summary>
-		/// 應用程式的主要進入點。
+		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		static void Main()
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault( false );
-			// 1. 先建立 View
-			FrmCoinExchange view = new FrmCoinExchange();
 
-			// 2. 建立 Presenter 並傳入 View
-			// 在 Presenter 的建構子中，必須包含：view.Presenter = this;
+			// initialize MVP components
+			FrmCoinExchange view = new FrmCoinExchange();
 			CoinExchangePresenter presenter = new CoinExchangePresenter( view );
 
-			// 3. 啟動視窗 (此時 view.Presenter 已經不是 null 了)
 			Application.Run( view );
 		}
 	}
